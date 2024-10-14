01bccaca34586340bce6a83c1616cd0426cdf08c
OTonGitHub <ifhaam@gcxnet.dev>
2024-10-14 06:00:28 +0500

    init commit, working static

---
+++ script.js
@@ -0,0 +1,23 @@
+const targetDate = new Date('October 12, 2024 00:08:00').getTime();
+
+const countdownInterval = setInterval(function () {
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
+
+  if (timeLeft < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById('countdown').innerHTML = '<h2>Starting Now..</h2>';
+  }
+}, 1000);
