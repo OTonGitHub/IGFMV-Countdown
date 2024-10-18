94aa04b93413772cc998490566546a213db5f263
OTonGitHub <ifhaam@gcxnet.dev>
2024-10-18 14:18:09 -0700

    correct time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdownTo = new Date('October 19, 2024 00:09:00').getTime();
-const targetDate = new Date(Date.UTC(2024, 9, 18, 19, 9, 0)); // October 19, 2024, 00:9:00 GMT+5
+const targetDate = new Date(Date.UTC(2024, 9, 19, 4, 0, 0)); // October 19, 2024, 00:09:00 GMT+5 (4AM UTC)
 
 const countdownInterval = setInterval(function () {
   const now = new Date().getTime();
