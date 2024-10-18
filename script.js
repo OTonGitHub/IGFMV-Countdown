9514de5d150ddf9c9c7af1ab69e5873ae4b47af6
OTonGitHub <ifhaam@gcxnet.dev>
2024-10-18 05:27:10 +0500

    remove days calculation

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const countdownInterval = setInterval(function () {
   const now = new Date().getTime();
   const timeLeft = targetDate - now;
 
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  document.getElementById('days').innerHTML = days.toString().padStart(2, '0');
   document.getElementById('hours').innerHTML = hours
     .toString()
     .padStart(2, '0');
